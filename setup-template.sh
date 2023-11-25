4a4d0e85988f2455cfd2f8ad99b47d6622fd72a2
Darius Drake <maverick.xero@proton.me>
2023-11-25 17:25:18 -0400

    Updated: added setup script

---
+++ setup-template.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -xe
+
+## Relpace these
+# ----------------- variables ------------------
+
+PROJECT_NAME="Example"
+PROJECT_ID="org.gnome.$PROJECT_NAME"
+RESOURCE_PATH="/org/gnome/$PROJECT_NAME"
+NAMESPACE="Example"
+CNAME="Example"
+AUTHOR="Developer"
+
+# -------------------- code --------------------
+
+# remove files that might conflict
+rm -rf build/ subprojects/blueprint-compiler/
+
+# remove git folder to unlink the new project from the template repository
+rm -rf .git/
+
+# rename files
+mv ./{org.gnome.Example,$PROJECT_ID}.json
+mv ./data/{org.gnome.Example,$PROJECT_ID}.metainfo.xml.in
+mv ./data/{org.gnome.Example,$PROJECT_ID}.desktop.in
+mv ./data/{org.gnome.Example,$PROJECT_ID}.gschema.xml
+mv ./data/icons/hicolor/scalable/apps/{org.gnome.Example,$PROJECT_ID}.svg
+mv ./data/icons/hicolor/symbolic/apps/{org.gnome.Example,$PROJECT_ID}-symbolic.svg
+mv ./src/{Example,$PROJECT_NAME}.gresource.xml
+
+# replace text in files
+find . -type f -name '*' -not -path './.git/*' -exec sed -i -e "s=/org/gnome/Example=$RESOURCE_PATH=g" {} \;
+find . -type f -name '*' -not -path './.git/*' -exec sed -i -e "s=org.gnome.Example=$PROJECT_ID=g" {} \;
+find . -type f -name '*' -not -path './.git/*' -exec sed -i -e "s=Example=$PROJECT_NAME=g" {} \;
+find . -type f -name '*' -not -path './.git/*' -exec sed -i -e "s=Example=$CNAME=g" {} \;
+find . -type f -name '*' -not -path './.git/*' -exec sed -i -e "s=Example=$NAMESPACE=g" {} \;
+find . -type f -name '*' -not -path './.git/*' -exec sed -i -e "s=Developer=$AUTHOR=g" {} \;
